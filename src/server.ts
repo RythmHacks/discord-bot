ba8d08957476c70bf794734b647a3091104ff23e
Li Feng Yin <lifeng-yin@proton.me>
2023-08-27 16:34:34 -0400

    fix: temp log invalid code issue to client

--- src/server.ts
+++ src/server.ts
@@ -84,7 +84,7 @@ app.post('/join-discord', async (req: Request, res: Response, next: NextFunction
 
         const accessToken = (await tokenResponse.json()).access_token
         if (tokenResponse.status === 400) {
-            return next(createHttpError(400, 'Invalid code.'))
+            return next(createHttpError(400, 'Invalid code: ' + JSON.stringify(tokenResponse)))
         }
         else if (!tokenResponse.ok || !accessToken) {
             throw createHttpError(tokenResponse.status, 'Failed to fetch access token from Discord.')
