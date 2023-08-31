8ebebe58747efedd07f1ff0840645aacf003a841
Jeffrey Zang <zangj4548@wrdsb.ca>
2023-08-31 18:58:44 -0400

    no more hooman monkeys

--- src/commands/monkey.ts
+++ src/commands/monkey.ts
@@ -8,17 +8,10 @@ module.exports = {
     async execute(interaction: ChatInputCommandInteraction) {
         await interaction.deferReply();
 
-        let choice = Math.floor(Math.random() * 25)
-
-        if (choice === 0) {
-            interaction.editReply("https://cdn.discordapp.com/attachments/1086658699201884321/1093953022318616636/Hooman.jpg")
-            return
-        } else {
-            const image: any = await fetch(
-                `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASHKEY}&query=monkey`
-            );
-            interaction.editReply((await image.json()).urls.full);
-            return 
-        }
+        const image: any = await fetch(
+            `https://api.unsplash.com/photos/random?client_id=${process.env.UNSPLASHKEY}&query=monkey`
+        );
+        interaction.editReply((await image.json()).urls.full);
+        return 
     },
 };
